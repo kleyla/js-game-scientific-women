55ce54f1b59543c4bc1dbe62d69d33bb124fe1cd
kleyla <kleyla01@live.com>
2021-08-22 02:03:12 -0400

    :sparkles: add some animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const scrollContainer = document.querySelector("#container");
 
   scrollContainer.addEventListener("wheel", (evt) => {
-    console.log("wheel");
     evt.preventDefault();
     scrollContainer.scrollLeft += evt.deltaY;
+  });
+
+  const startBtn = document.querySelector("#start-btn");
+  const startSection = document.querySelector("#start-section");
+  const main = document.querySelector("#container");
+
+  startBtn.addEventListener("click", () => {
+    startSection.classList.remove("animate__pulse");
+    startSection.classList.add("animate__backOutUp");
+
+    setInterval(() => {
+      startSection.remove();
+      main.classList.remove("hidden");
+      main.classList.add("animate__animated", "animate__zoomInUp");
+    }, 500);
   });
 });
